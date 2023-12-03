47336ce40d930d5e1fda41afe692931fbd502f5f
nikitamakovey <nikitamakovey@yahoo.com>
2023-12-03 13:54:07 +0700

    Hotfix

--- app/Http/Controllers/Api/FormController.php
+++ app/Http/Controllers/Api/FormController.php
@@ -6,9 +6,12 @@ use App\Http\Controllers\Controller;
 use App\Services\Auth\UserService;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Http;
 
 class FormController extends Controller
 {
+    const WEBHOOK_URL = 'https://forgehive.ru/webhook/';
+
     public function __construct(
         private readonly UserService $service
     )
@@ -20,7 +23,17 @@ class FormController extends Controller
         $attributes = $request->all();
         $user = $this->service->getUser();
         $user->update(['form' => json_encode($attributes)]);
-        // @todo: send to AI webhook
+
+        // GigaChat AI client
+        try {
+            $url = self::WEBHOOK_URL . $user->id;
+            $response = Http::post($url, $attributes);
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => $e->getMessage()
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
         return response()->json('', Response::HTTP_NO_CONTENT);
     }
 }
